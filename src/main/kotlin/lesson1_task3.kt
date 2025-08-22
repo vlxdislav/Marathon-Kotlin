fc220b4910a7f46a7dac6bfd2dfc43f641471ddf
Vladislav <prileosky.vladislav25@gmai.com>
2025-08-22 13:26:15 +0300

    MK-1-3-do-task

---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    val year : Int = 1961
+    var hour : Int = 9
+    var minute : Int = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+
+}
