f299a28f4515e050f317c87641f8478aaab2a18c
Vladislav <prileosky.vladislav25@gmai.com>
2025-08-23 19:16:00 +0300

    KS-1-5-do-task

--- src/main/kotlin/lesson1_task5.kt
+++ src/main/kotlin/lesson1_task5.kt
@@ -1,16 +1,13 @@
 fun main() {
+    val TOTAL_SECONDS: Int = 6480
 
-    val totalSeconds : Int = 6480
+    val SECONDS_IN_MINUTE = 60
+    val SECONDS_IN_HOUR = SECONDS_IN_MINUTE * SECONDS_IN_MINUTE
 
-    val hours = totalSeconds / 3600
-    val secondsAfterHours = totalSeconds % 3600
-    val minutes = secondsAfterHours / 60
-    val seconds = secondsAfterHours % 60
+    val hours = TOTAL_SECONDS / SECONDS_IN_HOUR
+    val secondsAfterHours = TOTAL_SECONDS % SECONDS_IN_HOUR
+    val minutes = secondsAfterHours / SECONDS_IN_MINUTE
+    val seconds = secondsAfterHours % SECONDS_IN_MINUTE
 
-    val formattedHours = String.format("%02d", hours)
-    val formattedMinutes = String.format("%02d", minutes)
-    val formattedSeconds = String.format("%02d", seconds)
-
-    println("$formattedHours:$formattedMinutes:$formattedSeconds")
-
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
